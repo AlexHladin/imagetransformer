8c8434ba37fbc3c5ee055f48f89a20268d6b48e0
Alex <alexgladin17@gmail.com>
2018-07-30 14:20:55 +0300

    Added upload dir creation

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const createError = require('http-errors');
+const path = require('path');
+const fs = require('fs');
 
 const middleware = require('./app/middleware');
 const swagger = require('./app/swagger');
 const routes = require('./app/routes');
 const mongo = require('./app/mongoose');
+const config = require('./config');
 
 const app = express();
 
 middleware(app, routes, swagger);
 mongo();
+
+const configPath = path.join(__dirname, config.uploadDir);
+if (!fs.existsSync(configPath)) {
+  fs.mkdirSync(configPath);
+}
 
 module.exports = app;
