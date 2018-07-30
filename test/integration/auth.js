afc0d578ff814a0767eacb606144fc4f7a8c60ad
Alex <alexgladin17@gmail.com>
2018-07-30 08:32:53 +0300

    Finished auth test

--- test/integration/auth.js
+++ test/integration/auth.js
@@ -1,32 +1,103 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const randomstring = require('randomstring');
 
-const User = require('../../app/model/user');
 const app = require('../../app');
 
 const should = chai.should();
 chai.use(chaiHttp);
 
+const random = (length = 10) => randomstring.generate({ length, readable: true, charset: 'alphabetic' });
+
 describe('Users', () => {
   /*
-   * Test the /POST route
+   * Test the signup route
    */
   describe('/POST signup', () => {
-    it('it should not POST a user without pages field', done => {
+    it('it should signup user', done => {
       const user = {
-        password: 'asd87asd',
-        email: 'asdasas12@gmail.com'
+        password: random(),
+        email: `${random()}@${random(5)}.${random(3)}`
       };
 
       chai.request(app)
-        .post('/api/auth/signup')
-        .send(user)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a('object');
+          .post('/api/auth/signup')
+          .send(user)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('authToken');
 
-          done();
-        });
+            done();
+          });
+    });
+
+    it('it should not signup user', done => {
+      chai.request(app)
+          .post('/api/auth/signup')
+          .send({})
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message');
+
+            done();
+          });
+    });
+  });
+
+  /*
+   * Test the signin route
+   */
+  describe('/POST signin', () => {
+    it('it should not signin user', done => {
+      const user = {
+        password: random(),
+        email: `${random()}@${random(5)}.${random(3)}`
+      };
+
+      chai.request(app)
+          .post('/api/auth/signin')
+          .send(user)
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message');
+
+            done();
+          });
+    });
+
+    /*
+     * Test the signin route
+     */
+    it('it should not signin user', done => {
+      const user = {
+        password: random(),
+        email: `${random()}@${random()}.${random(3)}`
+      };
+
+      console.log(user);
+
+      chai.request(app)
+          .post('/api/auth/signup')
+          .send(user)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('authToken');
+
+            chai.request(app)
+                .post('/api/auth/signin')
+                .send(user)
+                .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('object');
+                  res.body.should.have.property('authToken');
+
+                  done();
+                });
+          });
     });
   });
 });
